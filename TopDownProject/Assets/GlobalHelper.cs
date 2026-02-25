d65e90980b0fe06f363f0ce8e276cdd65d26345a
DiogoTadashi <diogotadashi22@gmail.com>
2026-02-25 15:59:58 -0300

    Arquivos antigos
    Removendo Arquivos antigos do projeto

--- TopDownProject/Assets/GlobalHelper.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class GlobalHelper : MonoBehaviour
-{
-    public static string GenerateUniqueID(GameObject obj)
-    {
-        return $"{obj.scene.name}_{obj.transform.position.x}_{obj.transform.position.y}";
-    }
-}
